87b0fe1ee05a4886506f4c2e95883319a6f61910
Carlos Gomez <isc.carlos.gomez@gmail.com>
2016-09-18 12:07:52 -0400

    Implement Rook#confirmMove

---
+++ chess/src/main/java/org/krloxz/chess/Rook.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2016 Carlos Gomez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.krloxz.chess;
+
+/**
+ * @author Carlos Gomez
+ */
+public class Rook extends Piece {
+
+    /**
+     * @param color
+     */
+    public Rook(final Color color) {
+        super(color);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.krloxz.chess.Piece#confirmLegalMove(org.krloxz.chess.Move, org.krloxz.chess.Board)
+     */
+    @Override
+    protected boolean confirmLegalMove(final Move move, final Board board) {
+        return move.getSource().getX() == move.getTarget().getX()
+                || move.getSource().getY() == move.getTarget().getY();
+    }
+
+}
---
+++ chess/src/test/java/org/krloxz/chess/RookTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2016 Carlos Gomez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.krloxz.chess;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit test {@link Rook}.
+ *
+ * @author Carlos Gomez
+ */
+public class RookTest {
+
+    private Rook rook;
+    private Board board;
+
+    @Before
+    public void setUp() {
+        this.rook = new Rook(Color.LIGHT);
+        this.board = mock(Board.class);
+    }
+
+    @Test
+    public void confirmLegalMove() {
+        assertTrue("Move should be legal", this.rook.confirmLegalMove(new Move("a1", "a8"), this.board));
+    }
+
+    @Test
+    public void confirmLegalMoveOnStraightMove() {
+        assertFalse("Move should be ilegal", this.rook.confirmLegalMove(new Move("a1", "h8"), this.board));
+    }
+
+}

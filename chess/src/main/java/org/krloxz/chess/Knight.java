d440e7d97026bf00548c7f0c1fded6b1f3a433d5
Carlos Gomez <isc.carlos.gomez@gmail.com>
2016-09-18 12:35:36 -0400

    Implement Knight#confirmMove

---
+++ chess/src/main/java/org/krloxz/chess/Knight.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2016 Carlos Gomez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.krloxz.chess;
+
+/**
+ * @author Carlos Gomez
+ */
+public class Knight extends Piece {
+
+    /**
+     * @param color
+     */
+    public Knight(final Color color) {
+        super(color);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.krloxz.chess.Piece#confirmLegalMove(org.krloxz.chess.Move, org.krloxz.chess.Board)
+     */
+    @Override
+    protected boolean confirmLegalMove(final Move move, final Board board) {
+        final int xLength = Math.abs(move.getTarget().getX() - move.getSource().getX());
+        final int yLength = Math.abs(move.getTarget().getY() - move.getSource().getY());
+        return xLength == 1 && yLength == 2
+                || xLength == 2 && yLength == 1;
+    }
+
+}
---
+++ chess/src/test/java/org/krloxz/chess/KnightTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2016 Carlos Gomez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.krloxz.chess;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit test {@link Knight}.
+ *
+ * @author Carlos Gomez
+ */
+public class KnightTest {
+
+    private Knight knight;
+    private Board board;
+
+    @Before
+    public void setUp() {
+        this.knight = new Knight(Color.LIGHT);
+        this.board = mock(Board.class);
+    }
+
+    @Test
+    public void confirmLegalMoveOnVerticalLength() {
+        assertTrue("Move should be legal", this.knight.confirmLegalMove(new Move("a1", "b3"), this.board));
+    }
+
+    @Test
+    public void confirmLegalMoveOnHorizontalLength() {
+        assertTrue("Move should be legal", this.knight.confirmLegalMove(new Move("a1", "c2"), this.board));
+    }
+
+    @Test
+    public void confirmLegalMoveOnNotLMove() {
+        assertFalse("Move should be ilegal", this.knight.confirmLegalMove(new Move("a1", "h8"), this.board));
+    }
+
+}
